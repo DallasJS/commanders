bb1d2d896416965be37e7ca9700ed6b322fadaae
Alexis Abril <me@alexisabril.com>
2013-07-31 22:35:06 -0500

    Adding configs

--- public/commanders.js
+++ public/commanders.js
@@ -1,10 +1,3 @@
-require.config({
-	paths: {
-		'can': 'lib/canjs/amd/can',
-		'jquery': 'lib/jquery/jquery'
-	}
-});
-
 require(['main'], function(Main) {
 	new Main('#main');
 });
---
+++ public/config.js
@@ -0,0 +1,7 @@
+require.config({
+	deps: ['commanders'],
+	paths: {
+		'can': 'lib/canjs/amd/can',
+		'jquery': 'lib/jquery/jquery'
+	}
+});
