085e5d897951649ee8f5a49dafd88deda5c10a08
Alexis Abril <dev@alexisabril.com>
2014-04-02 18:02:58 -0500

    main app working

--- public/commanders.js
+++ public/commanders.js
@@ -16,104 +16,73 @@
 		})
 	});
 
-	// 	'{Commander} updated': function() {
-	// 		this.reorder();
-	// 	},
-
-	var commandersVM = can.Map.extend({
-		init: function() {
-			var self = this;
-
-			Commander.findAll({}, function(commanders) {
-				self.attr('commanders', commanders);
-			});
-		},
-
-		favorites: new Commander.List(),
-		tooltip: null,
-
-		reorder: function() {
-			var rows = $(this.element.find('tr:gt(0)'));
-
-			Array.prototype.sort.call(rows, function(a, b) {
-				var rowA = $(a),
-						rowB = $(b),
-						modelA = rowA.data('commander'),
-						modelB = rowB.data('commander');
-
-				if(modelA.attr('votes') < modelB.attr('votes')) {
-					rowA.before(rowB);
-
-					return 1;
-				}
-				else if(modelB.attr('votes') < modelA.attr('votes')) {
-					rowB.before(rowA);
-
-					return -1;
-				}
-
-				return 0;
-			});
-		}
-	});
-
 	can.Component.extend({
-		tag: 'commanders',
+		tag: 'commanders-list',
 		template: can.view('main.mustache'),
-		scope: commandersVM,
-		events: {
-			'{Commander} updated': function() {
-				// this.scope.reorder();
-			},
+		scope: {
+			commanders: new Commander.List(),
 
-			'.up click': function(el, ev) {
-				var commander = el.closest('tr').data('commander');
+			favorites: new Commander.List(),
+
+			tooltipCommander: null,
+			Commander: Commander,
+
+			upvote: function(commander) {
 				commander.attr('upvotes', commander.upvotes + 1).save();
 			},
 
-			'.down click': function(el, ev) {
-				var commander = el.closest('tr').data('commander');
+			downvote: function(commander) {
 				commander.attr('downvotes', commander.downvotes + 1).save();
 			},
 
-			'.favorite click': function(el, ev) {
-				this.scope.favorites.push(el.closest('tr').data('commander'));
+			favorite: function(commander, el) {
+				this.attr('favorites').push(commander);
 				el.remove();
 			},
 
-			'.delete click': function(el, ev) {
-				el.closest('tr').data('commander').destroy();
+			delete: function(commander) {
+				commander.destroy();
 			},
 
-			'.photo mouseenter': function(el, ev) {
-				// var offset = $(options.anchor).offset();
-				// 	el.appendTo(document.body)
-				// 		.offset( {
-				// 			left: offset.left,
-				// 			top: offset.top - 32
-				// 		}).animate({ opacity : 1 });
-				var commander = el.closest('tr').data('commander');
-				// this.scope.show(true);
-				console.log('setting')
-				this.scope.attr('tooltip', commander);
+			mouseenter: function(commander) {
+				this.attr('tooltipCommander', commander);
+			},
+
+			reorder: function() {
+				var commanders = this.attr('commanders');
+
+				commanders.comparator = 'votes';
+				var sorted = commanders.sort().reverse();
+
+				commanders.replace(can.makeArray(sorted));
+			}
+		},
+		events: {
+			'inserted': function() {
+				console.log('inserted')
+				var scope = this.scope;
+				Commander.findAll({}, function(commanders) {
+					scope.attr('commanders', commanders);
+					scope.reorder();
+				});
+			},
+
+			'{Commander} updated': function() {
+				this.scope.reorder();
 			}
 		}
 	});
 
 	can.Component.extend({
-		tag: 'tooltip',
+		tag: 'commander-tooltip',
 		template: can.view('tooltip.mustache'),
-		scope: {
-			name: 'foo',
-		},
+		scope: {},
 		events: {
-			'{scope} tooltip': function(o, ev, val, old) {
+			'{scope} commander': function(o, ev, val, old) {
 				console.log('show', arguments);
-
-				
 			}
 		}
-	})
+	});
 
 	$('#main').html(can.view('app.mustache', {}));
 
